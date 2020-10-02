21b68c6a4e4f0329ddcb15f4694c784eb1860477
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2020-10-02 16:58:11 +0300

    hello world unity

---
+++ Assets/script1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script1 : MonoBehaviour
+{
+    public GameObject cube;
+    private Transform cubeTransform;
+    private float zPosition = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeTransform = cube.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cubeTransform.position = new Vector3(0, 0, zPosition);
+        zPosition += 0.0001f;
+    }
+}
